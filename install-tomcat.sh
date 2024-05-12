12ee403937a68b62bd0758bee62653b0e70239b9
tranju664 <164826479+tranju664@users.noreply.github.com>
2024-05-12 19:33:14 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -2,14 +2,14 @@
 ver=`curl --silent http://mirror.vorboss.net/apache/tomcat/tomcat-9/ | grep v9 | awk '{split($5,c,">v") ; split(c[2],d,"/") ; print d[1]}'`
 sudo yum install -y java wget git
 cd /opt
-sudo rm -rf apache*
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v$ver/bin/apache-tomcat-$ver.tar.gz
-sudo tar xvzf apache-tomcat-9.*
-sudo rm -rf apache*.gz
-sudo git clone https://github.com/adhig93/tomcat-config
-sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
-sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
-sudo cp ./tomcat-config/tomcat-users.xml /opt/apache-tomcat-9.*/conf/tomcat-users.xml
-sudo rm -rf tomcat-config
+rm -rf apache*
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v$ver/bin/apache-tomcat-$ver.tar.gz
+tar xvzf apache-tomcat-9.*
+rm -rf apache*.gz
+git clone https://github.com/adhig93/tomcat-config
+cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
+cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
+cp ./tomcat-config/tomcat-users.xml /opt/apache-tomcat-9.*/conf/tomcat-users.xml
+rm -rf tomcat-config
 cd /opt/apache-tomcat-9.*
-sudo sh bin/startup.sh
+sh bin/startup.sh
